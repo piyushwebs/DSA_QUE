34d6dbb64bbc815467ce9546944369cb9d9f2cee
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-07-06 09:01:52 +0530

    Time: 753 ms (98.05%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select prices.product_id,(case when ROUND(sum(prices.price*unitssold.units)/(case when sum(unitssold.units) is null then 1 else sum(unitssold.units) end),2) is null then 0 else  ROUND(sum(prices.price*unitssold.units)/(case when sum(unitssold.units) is null then 1 else sum(unitssold.units) end),2) end) as average_price from prices 
+select prices.product_id,coalesce(ROUND(sum(prices.price*unitssold.units)/(sum(unitssold.units)),2),0) as average_price from prices 
 left join 
 unitssold 
 on prices.product_id = unitssold.product_id and prices.start_date<=unitssold.purchase_date and prices.end_date>=unitssold.purchase_date
