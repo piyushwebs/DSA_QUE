b359fa2a12ed525cfb2dedd69bb42f221e987945
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-07-15 19:16:49 +0530

    Time: 3 ms (99.78%) | Memory: 47.7 MB (6.18%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,42 +1,53 @@
 class Solution {
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int n = prerequisites.length;
-        if (n == 0)
-            return true;
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            if (hm.containsKey(prerequisites[i][1])) {
-                ArrayList<Integer> arr = hm.get(prerequisites[i][1]);
-                for (int j = 0; j < arr.size(); j++) {
-                    if (arr.get(j) == prerequisites[i][0])
-                        return false;
-                }
-                    ArrayList<Integer> list1 = new ArrayList<>();
-                    list1.add(prerequisites[i][1]);
-                hm.put(prerequisites[i][0],list1);
-            } else {
-                if (hm.containsKey(prerequisites[i][0])) {
-                    hm.get(prerequisites[i][0]).add(prerequisites[i][1]);
-                } else {
-                    ArrayList<Integer> list = new ArrayList<>();
-                    list.add(prerequisites[i][1]);
-                    hm.put(prerequisites[i][0], list);
-                }
-            }
+
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
         }
-        Arrays.sort(prerequisites, (a, b) -> Integer.compare(a[1], b[1]));
-        for (int i = 0; i < n; i++) {
-            int val1 = prerequisites[i][0];
-            int val2 = prerequisites[i][1];
-            if (val1 == val2)
-                return false;
-            for (int j = 0; j < n; j++) {
-                if (val2 == prerequisites[j][0]) {
-                    prerequisites[j][0] = val1;
-                }
+
+        // Build graph
+        for (int[] edge : prerequisites) {
+            graph[edge[1]].add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[numCourses];
+        boolean[] pathVisited = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (dfs(i, graph, visited, pathVisited))
+                    return false;
             }
         }
 
         return true;
+    }
+
+    private boolean dfs(int node, ArrayList<Integer>[] graph,
+                        boolean[] visited,
+                        boolean[] pathVisited) {
+
+        visited[node] = true;
+        pathVisited[node] = true;
+
+        for (int neighbour : graph[node]) {
+
+            if (!visited[neighbour]) {
+
+                if (dfs(neighbour, graph, visited, pathVisited))
+                    return true;
+
+            } else if (pathVisited[neighbour]) {
+
+                return true;
+            }
+        }
+
+        pathVisited[node] = false;
+
+        return false;
     }
 }
