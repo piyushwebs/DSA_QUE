71df531db189381e2abbd14071b1ebff4bce1b0c
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-07-22 06:45:40 +0530

    Time: 18 ms (3.44%) | Memory: 47 MB (5.82%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int minutes = 0;
+
+        Set<List<Integer>> st = new HashSet<>();
+        Queue<List<Integer>> q = new LinkedList<>();
+
+       
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    List<Integer> arr = new ArrayList<>();
+                    arr.add(i);
+                    arr.add(j);
+
+                    st.add(arr);
+                }
+                else if(grid[i][j] == 2)
+                {
+                    List<Integer> arr = new ArrayList<>();
+                    arr.add(i);
+                    arr.add(j);
+
+                    q.offer(arr);
+                }
+            }
+        }
+
+
+         System.out.println("1 : "+st);
+
+         System.out.println("2 :" + q);
+
+        if(st.size() == 0)
+        {
+            return 0;
+        }
+
+        while(!q.isEmpty() && !st.isEmpty())
+        {
+            int itr = q.size();
+            minutes++;
+            for(int i=0;i<itr;i++)
+            {
+                List<Integer> arr = q.poll();
+
+                int[] a = {arr.get(0),arr.get(1)-1,arr.get(0),arr.get(1)+1,arr.get(0)+1,arr.get(1),arr.get(0)-1,arr.get(1)};
+              
+                int j = 0;
+                while(j<8)
+                {
+                    List<Integer> temp = new ArrayList<>(Arrays.asList(a[j],a[j+1]));
+                    if(st.contains(temp))
+                    {
+                        q.offer(temp);
+                        st.remove(temp);
+                    }
+                    j+=2;
+                }
+            }
+        }
+        if(st.size()!=0) 
+        {
+            return -1;
+        }
+        return minutes;
+    }
+}
