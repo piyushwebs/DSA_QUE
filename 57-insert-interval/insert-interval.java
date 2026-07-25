a6c49c19edb41dda6ce8d2dcf3de70e26ffb29ef
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-07-25 18:10:04 +0530

    Time: 1 ms (98.10%) | Memory: 47.1 MB (36.86%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,147 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
         int n = intervals.length;
 
-        int currStart = 0;
-        int currEnd = 0;
-
-        List<List<Integer>> arr = new ArrayList<>();
-        boolean flag = true;
-
-        while(currEnd<n)
-        {
-          List<Integer> temp = new ArrayList<>();
-          if( currEnd == 0 && newInterval[1]<intervals[currEnd][0])
-          {
-            flag = false;
-            break;
-          }
-          while(currEnd<n && intervals[currEnd][1]<newInterval[0])
-          {  
-            currEnd++;
-          }
-          
-          if(currEnd==n)
-          {
-            break;
-          }
-          else if(intervals[currEnd][0]<=newInterval[1])
-          {
-             int val1 = Math.min(intervals[currEnd][0],newInterval[0]);
-             int val2 = Math.max(intervals[currEnd][1],newInterval[1]);
-
-             intervals[currEnd][0] = val1;
-             intervals[currEnd][1] = val2;
-          }
-          else
-          {
-            flag = false;
-            break;
-          }
-          break;
-        }
-         
-
-        if(!flag)
-        {
-            currEnd = 0;
-            currStart = 0;
-
-            while(currEnd<n)
-            {
-                currStart = currEnd;
-                int min = intervals[currStart][0];
-                int max = intervals[currStart][1];
-               
-                if(newInterval[1]<min && !flag)
-                {
-                    List<Integer> temp1 = new ArrayList<>();
-                    temp1.add(newInterval[0]);
-                    temp1.add(newInterval[1]);
-
-                    arr.add(temp1);
-                    flag = true;
-                }
-
-                List<Integer> temp = new ArrayList<>();
-
-                temp.add(min);
-                temp.add(max);
-
-                arr.add(temp);
-                currEnd++;
-            }
-        }
-        else if(currEnd != n)
-        {
-            currEnd = 0;
-            currStart = 0;
-        
-            while(currEnd<n)
-            {
-                
-                int min = intervals[currEnd][0];
-                int max = intervals[currEnd][1];
-                while(currEnd<n-1 && intervals[currStart][1] >= intervals[currEnd+1][0])
-                {
-                    min = Math.min(min,intervals[currEnd+1][0]);
-                    max = Math.max(max,intervals[currEnd+1][1]);
-                    if(intervals[currStart][1]<intervals[currEnd+1][1])
-                    {
-                        currStart = currEnd+1;
-                    }
-                    currEnd++;
-                }
-                List<Integer> temp = new ArrayList<>();
-
-                temp.add(min);
-                temp.add(max);
-
-                arr.add(temp);
-                currEnd++;
-                currStart = currEnd;
-            }
-        }
-        else
-        {
-            currEnd = 0;
-            currStart = 0;
-        
-            while(currEnd<n)
-            {
-                currStart = currEnd;
-                int min = intervals[currStart][0];
-                int max = intervals[currStart][1];
-               
-                List<Integer> temp = new ArrayList<>();
-
-                temp.add(min);
-                temp.add(max);
-
-                arr.add(temp);
-                currEnd++;
-            }
-
-            List<Integer> temp1 = new ArrayList<>();
-            temp1.add(newInterval[0]);
-            temp1.add(newInterval[1]);
-            
-            arr.add(temp1);
+        // Step 1: Add all intervals completely before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
         }
 
-        
-
-        int m = arr.size();
-        int[][] ans = new int[m][2];
-
-        for(int i=0;i<m;i++)
-        {
-            List<Integer> temp = arr.get(i);
-
-            ans[i][0] = temp.get(0);
-            ans[i][1] = temp.get(1);
+        // Step 2: Merge all overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
 
-        return ans;
+        // Add the merged interval
+        result.add(newInterval);
+
+        // Step 3: Add remaining intervals
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // Convert List<int[]> to int[][]
+        return result.toArray(new int[result.size()][]);
     }
 }
