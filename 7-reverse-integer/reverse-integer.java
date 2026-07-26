26e840a178204bb19913fdd1a4758695fcf95a2d
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-07-26 11:58:04 +0530

    Time: 7 ms (2.08%) | Memory: 42.9 MB (11.40%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int reverse(int x) {
+      
+        int sign = 1;
+        if(x<0)
+        {
+            x = x*-1;
+            sign = -1;
+        }
+
+        if(x == Integer.MIN_VALUE ||  x==Integer.MAX_VALUE)
+        {
+            return 0;
+        }
+        int ans = 0;
+        
+        boolean flag = false;
+
+        StringBuilder str = new StringBuilder("");
+
+        int limit = Integer.MAX_VALUE;
+
+        if(x==0)
+        {
+                return 0;
+        }
+
+        while(x!=0)
+        {
+            while(!flag &&  x!=0 && x%10 == 0)
+            {
+                x = x/10;
+                
+            }
+            flag = true;
+            
+            int val = x%10;
+            if(val<0)
+            {
+                val = val*-1;
+            }
+            str.append(val);
+            x = x/10;
+        }
+        System.out.println(str);
+        long val = Long.parseLong(str.toString());
+        if(val>limit || val<Integer.MIN_VALUE)
+        {
+            return 0;
+        }
+        ans = (int)val;
+       return ans*sign;
+    }
+}
