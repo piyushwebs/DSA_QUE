608dcae6f01b94c97a2f0fb8e9396d68f48510cf
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-07-31 22:12:06 +0530

    Time: 15 ms (3.38%) | Memory: 43.1 MB (63.61%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,42 @@
+class Solution {
+    int ans = 0;
+    public int numDecodings(String s) {
+        int n = s.length();
+        if(s.charAt(0) == '0') return 0;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,0);
+        decode(s,n,0,dp);
+        return dp[0];
+    }
+    public int decode(String s, int n, int start,int[] dp)
+    {
+        if((start) == n)
+        { 
+            return 1;
+        }
+        if(start>=n || s.charAt(start) == '0' )
+        {
+            return 0;
+        }
+        if(dp[start] != 0)
+        {
+            return dp[start];
+        }
+    
+        dp[start] += decode(s,n,start+1,dp);
+
+        if(start+1>=n)
+        {
+            return dp[start];
+        }
+        int num = (((s.charAt(start)-'0')*10) + (s.charAt(start+1)-'0')) ;
+
+        if(num>=10 && num <= 26)
+        {
+             dp[start] += decode(s,n,start+2,dp);
+        }
+        
+       
+        return dp[start];
+    }
+}
