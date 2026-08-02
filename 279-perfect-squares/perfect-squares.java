e6242d536c05a0c954c56846e991d55c330c435f
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-08-02 17:21:26 +0530

    Time: 221 ms (7.65%) | Memory: 44.8 MB (26.24%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int numSquares(int n) {
+        int val = (int)Math.sqrt(n);
+        if(val*val == n)
+        {
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+
+        for(int i=1;Math.pow(i,2)<n;i++)
+        {
+            dp[n] = Math.min(dp[n],checkPow(n,(int)Math.pow(i,2),dp));
+        }
+        return dp[n]+1;
+    }
+    public int checkPow(int n,int sum,int[] dp)
+    {
+        
+        if(sum>n)
+        {
+            return Integer.MAX_VALUE;
+        }
+        
+        if(sum == n)
+        {
+            return 0;
+        }
+        if(dp[sum]!=Integer.MAX_VALUE)
+        {
+            return dp[sum];
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i=1;Math.pow(i,2)<n;i++)
+        {
+            int rem = checkPow(n,sum+(int)Math.pow(i,2),dp);
+            if(rem!=Integer.MAX_VALUE)
+            {
+                ans = Math.min(ans,rem+1);
+            }
+        }
+        dp[sum] = ans;
+        return dp[sum];
+    }
+}
