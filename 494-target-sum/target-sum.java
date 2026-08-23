8c1b713d74e49380c1bd469d7970cfa03af007b5
PiyushBhoyar <153488419+piyushwebs@users.noreply.github.com>
2026-08-23 08:40:17 +0530

    Time: 427 ms (29.81%) | Memory: 43.7 MB (54.79%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,37 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        return numWays(nums,target,n,0,0,0);
+        int val = 0;
+        if(target>=0)
+        {
+            val = target;
+        }
+        int[][] dp = new int[n][val];
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+        return numWays(nums,target,n,0,0,0,dp);
     }
-    public int numWays(int[] nums,int target,int n,int sum,int idx,int total)
+    public int numWays(int[] nums,int target,int n,int sum,int idx,int total,int[][] dp)
     {
         if(idx == n )
         {
             return sum==target?1:0;
         }
+        if(sum>=0 && sum<target && dp[idx][sum]!=-1)
+        {
+            return dp[idx][sum];
+        }
        
-        int minus = numWays(nums,target,n,sum-(nums[idx]),idx+1,total);
-        int plus = numWays(nums,target,n,sum+nums[idx],idx+1,total);
+        int minus = numWays(nums,target,n,sum-(nums[idx]),idx+1,total,dp);
+        int plus = numWays(nums,target,n,sum+nums[idx],idx+1,total,dp);
         
+        if(sum>=0 && sum<target)
+        {
+            dp[idx][sum] = minus+plus;
+        }
+
         return minus+plus;
     }
 }
